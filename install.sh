ee85f0c13aa055865397c3829bbf5647f81f5a04
tlichten <tlichtenstein@mirantis.com>
2017-05-09 17:58:40 +0200

    updating model

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-
-# Bootstrap nodes
-salt -C 'I@salt:master' state.sls linux
-salt -C 'I@salt:master' state.sls salt.master,reclass
-salt -C 'I@salt:master' state.sls salt.minion
-salt '*' state.sls linux,openssh,ntp,salt.minion
